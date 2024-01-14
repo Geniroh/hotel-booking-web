94eb9be3c29455b5c09f1f62e9227a0db330d676
Geniroh2 <chibuzor@codematic.io>
2024-01-14 12:48:55 +0100

    first stage complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,36 @@
-// document.addEventListener('DOMContentLoaded', function(){
-//     function checkWindowSize() {
-//         const windowWidth = window.innerWidth;
-//         return windowWidth < 1000 ? 1 : 2;
-//     }
-      
-//     function handleResize() {
-//         const result = checkWindowSize();
-//     }
-      
-//     window.addEventListener('resize', handleResize);
-      
-//     flatpickr(".my-calendar", {
-//         inline: true,
-//         mode: "range",
-//         showMonths:  handleResize,
-//         // wrap: true
-//     });
-// })
-
 document.addEventListener('DOMContentLoaded', function(){
     function checkWindowSize() {
         const windowWidth = window.innerWidth;
         return windowWidth < 1000 ? 1 : 2;
     }
-
+  
+    const configObj = {
+        inline: true,
+        mode: "range",
+        dateFormat: "Y-m-d",
+        disable: [
+        {
+            from: "2024-01-13",
+            to: "2024-01-17"
+        },
+        {
+            from: "2025-09-01",
+            to: "2025-12-01"
+        }
+        ]
+    };
+  
     function handleResize() {
         const result = checkWindowSize();
-        flatpickr(".my-calendar", {
-            inline: true,
-            mode: "range",
-            showMonths: result,
-                dateFormat: "Y-m-d",
-                disable: [
-                    {
-                        from: "2024-01-13",
-                        to: "2024-01-17"
-                    },
-                    {
-                        from: "2025-09-01",
-                        to: "2025-12-01"
-                    }
-                ]
-        });
+        configObj.showMonths = result;
     }
-
     window.addEventListener('resize', handleResize);
 
     handleResize();
-    function testfunc () {
+    
+    flatpickr(".my-calendar", configObj);
+
+    function splideInit () {
         new Splide('#thumbnail-carousel', {
             fixedWidth : 100,
             fixedHeight: 60,
@@ -65,87 +48,51 @@ document.addEventListener('DOMContentLoaded', function(){
         } ).mount();
     }
 
-    // document.addEventListener( 'DOMContentLoaded', function () {
-    //     var main = new Splide( '#main-carousel', {
-    //       type      : 'fade',
-    //       rewind    : true,
-    //       pagination: false,
-    //       arrows    : false,
-    //     } );
-      
-      
-    //     var thumbnails = new Splide( '#thumbnail-carousel', {
-    //       fixedWidth  : 100,
-    //       fixedHeight : 60,
-    //       gap         : 10,
-    //       rewind      : true,
-    //       pagination  : false,
-    //       isNavigation: true,
-    //       breakpoints : {
-    //         600: {
-    //           fixedWidth : 60,
-    //           fixedHeight: 44,
-    //         },
-    //       },
-    //     } );
-      
-      
-    //     main.sync( thumbnails );
-    //     main.mount();
-    //     thumbnails.mount();
-    //   } );
-
     var splide = new Splide( '#main-carousel', {
         pagination: false,
-      } );
+    } );
+      
+    var thumbnails = document.getElementsByClassName( 'thumbnail' );
+    var current;
       
       
-      var thumbnails = document.getElementsByClassName( 'thumbnail' );
-      var current;
-      
-      
-      for ( var i = 0; i < thumbnails.length; i++ ) {
+    for ( var i = 0; i < thumbnails.length; i++ ) {
         initThumbnail( thumbnails[ i ], i );
-      }
+    } 
       
-      
-      function initThumbnail( thumbnail, index ) {
+    function initThumbnail( thumbnail, index ) {
         thumbnail.addEventListener( 'click', function () {
           splide.go( index );
         } );
-      }
+    }
       
-      
-      splide.on( 'mounted move', function () {
+    splide.on( 'mounted move', function () {
         var thumbnail = thumbnails[ splide.index ];
-      
-      
+        
+        
         if ( thumbnail ) {
-          if ( current ) {
-            current.classList.remove( 'is-active' );
-          }
-      
-      
-          thumbnail.classList.add( 'is-active' );
-          current = thumbnail;
+            if ( current ) {
+                current.classList.remove( 'is-active' );
+            }
+        
+            thumbnail.classList.add( 'is-active' );
+            current = thumbnail;
         }
-      } );
+    });
       
-      
-      splide.mount();
+    splide.mount();
 
-        setTimeout(function() {
-          hideLoadingRooms();
-        }, 2000); 
-      
-        function hideLoadingRooms() {
-          var loadingElement = document.querySelector('#loadingScreen');
-      
-          if (loadingElement) {
-            loadingElement.classList.add('hidden');
-          }
+    // Spinner
+    function hideLoadingRooms() {
+        var loadingElement = document.querySelector('#loadingScreen');
+    
+        if (loadingElement) {
+        loadingElement.classList.add('hidden');
         }
-
+    }
+    
+    setTimeout(function() {
+        hideLoadingRooms();
+    }, 2000); 
       
-    // testfunc()
 });
